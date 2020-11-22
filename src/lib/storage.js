c7da2b17285629d6751bd12e3d4a0a411e6f4917
Jana <janasundar1997@gmail.com>
2020-11-22 16:56:15 +0530

    new sprite added

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -1,17 +1,19 @@
-import ScratchStorage from 'scratch-storage';
+import ScratchStorage from "scratch-storage";
 
-import defaultProject from './default-project';
+import defaultProject from "./default-project";
+
+const excludedItems = ["f64042743297db677caa7f514010852c"];
 
 /**
  * Wrapper for ScratchStorage which adds default web sources.
  * @todo make this more configurable
  */
 class Storage extends ScratchStorage {
-    constructor () {
+    constructor() {
         super();
         this.cacheDefaultProject();
     }
-    addOfficialScratchWebStores () {
+    addOfficialScratchWebStores() {
         this.addWebStore(
             [this.AssetType.Project],
             this.getProjectGetConfig.bind(this),
@@ -19,7 +21,11 @@ class Storage extends ScratchStorage {
             this.getProjectUpdateConfig.bind(this)
         );
         this.addWebStore(
-            [this.AssetType.ImageVector, this.AssetType.ImageBitmap, this.AssetType.Sound],
+            [
+                this.AssetType.ImageVector,
+                this.AssetType.ImageBitmap,
+                this.AssetType.Sound,
+            ],
             this.getAssetGetConfig.bind(this),
             // We set both the create and update configs to the same method because
             // storage assumes it should update if there is an assetId, but the
@@ -29,56 +35,64 @@ class Storage extends ScratchStorage {
         );
         this.addWebStore(
             [this.AssetType.Sound],
-            asset => `static/extension-assets/scratch3_music/${asset.assetId}.${asset.dataFormat}`
+            (asset) =>
+                `static/extension-assets/scratch3_music/${asset.assetId}.${asset.dataFormat}`
         );
     }
-    setProjectHost (projectHost) {
+    setProjectHost(projectHost) {
         this.projectHost = projectHost;
     }
-    getProjectGetConfig (projectAsset) {
+    getProjectGetConfig(projectAsset) {
         return `${this.projectHost}/${projectAsset.assetId}`;
     }
-    getProjectCreateConfig () {
+    getProjectCreateConfig() {
         return {
             url: `${this.projectHost}/`,
-            withCredentials: true
+            withCredentials: true,
         };
     }
-    getProjectUpdateConfig (projectAsset) {
+    getProjectUpdateConfig(projectAsset) {
         return {
             url: `${this.projectHost}/${projectAsset.assetId}`,
-            withCredentials: true
+            withCredentials: true,
         };
     }
-    setAssetHost (assetHost) {
+    setAssetHost(assetHost) {
         this.assetHost = assetHost;
     }
-    getAssetGetConfig (asset) {
+    getAssetGetConfig(asset) {
+        console.log(asset);
+        if (excludedItems.includes(asset.assetId)) {
+            console.log("Hello");
+            return `/static/${asset.assetId}.${asset.dataFormat}`;
+        }
         return `${this.assetHost}/internalapi/asset/${asset.assetId}.${asset.dataFormat}/get/`;
     }
-    getAssetCreateConfig (asset) {
+    getAssetCreateConfig(asset) {
         return {
             // There is no such thing as updating assets, but storage assumes it
             // should update if there is an assetId, and the asset store uses the
             // assetId as part of the create URI. So, force the method to POST.
             // Then when storage finds this config to use for the "update", still POSTs
-            method: 'post',
+            method: "post",
             url: `${this.assetHost}/${asset.assetId}.${asset.dataFormat}`,
-            withCredentials: true
+            withCredentials: true,
         };
     }
-    setTranslatorFunction (translator) {
+    setTranslatorFunction(translator) {
         this.translator = translator;
         this.cacheDefaultProject();
     }
-    cacheDefaultProject () {
+    cacheDefaultProject() {
         const defaultProjectAssets = defaultProject(this.translator);
-        defaultProjectAssets.forEach(asset => this.builtinHelper._store(
-            this.AssetType[asset.assetType],
-            this.DataFormat[asset.dataFormat],
-            asset.data,
-            asset.id
-        ));
+        defaultProjectAssets.forEach((asset) =>
+            this.builtinHelper._store(
+                this.AssetType[asset.assetType],
+                this.DataFormat[asset.dataFormat],
+                asset.data,
+                asset.id
+            )
+        );
     }
 }
 
